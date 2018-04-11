7053aa8bd97e8b2a1a25f5383c1b4b9e96ee1161
Marie Barr-Ramsey <0751544@my.scccd.edu>
2018-04-11 13:54:26 -0700

    Fixing github errors..

--- main.cc
+++ main.cc
@@ -90,21 +90,7 @@ int main(){
 		cout << "b) guess the combo" << endl; 
 		cout << "c) clip the lock off with some industrial lock cutters" << endl; 
 
-<<<<<<< HEAD
-=======
-/*
-//PLAYER 1'S RIDDLE
-cout<<"What color are eggs? (riddle)"<<endl;
->>>>>>> 4c618998dc5351c112c1891a25755c36e1d83710
-string ans;
-cin>>ans;
-if(ans=="white" or ans=="White"){
-	int score=money.loot();
-	map.at(it->first).set_score(score);
-	cout<<"SCORE: "<<score<<endl;
-}
-else cout<<"no cigar."<<endl;
-*/ 
+ 
 cout << "LEVEL 1: SHALLOW FIN-ANCES" << endl; 
 cout << "PLAYER 2: SIT TIGHT AND BE READY TO DRIVE." << endl; 
 cout << "PLAYER 1: YOUR FIRST TASK IS TO ROB A LOAN SHARK IN THE DEAD OF NIGHT. EASY, RIGHT?" << endl; 
@@ -126,7 +112,6 @@ cout << "c) clip the lock off with some industrial lock cutters" << endl;
 	if(choice == 'a') { 
 		cout << "You accidentally burn away some of the money." << endl; 
 		//collect da money
->>>>>>> 230396e6d64d9e708be835e1855fe32a182976b8
 		money_value=rand()%100000+150;
 		Treasure money1(0,x_treas,0,0,money_value);
 		int score1=money1.loot();
@@ -170,51 +155,12 @@ cout << "c) clip the lock off with some industrial lock cutters" << endl;
 			map.at(it1->first).set_score(score1);
 			cout << "Money collected: " << "$" <<score1  << endl; //fix to actual
 		}
-<<<<<<< HEAD
 	}	
-
-=======
-	}
-}
-	else if(choice == 'c') cout << "Money collected: " << "$" << amount  << endl; //fix to actual
-
 }	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-money_value=rand()%100000+150;
-Treasure money2(0,x_treas,0,0,money_value);
-//PLAYER 2'S RIDDLE
-cout<<"What color are eggs? (riddle)"<<endl;
-cin>>ans;
-if(ans=="white" or ans=="White"){
-	    int score2=money2.loot();
-	//	cout<<score2;
-		map.at(it2->first).set_score(score2);
-		cout<<"SCORE: "<<score2<<endl;
-}
-else cout<<"no cigar."<<endl;
->>>>>>> 230396e6d64d9e708be835e1855fe32a182976b8
-
-	
 
 /*-------------------GETAWAY PART------------------------------*/	
 
-<<<<<<< HEAD
 //PLAYER 2's PART
-=======
 cout << "PLAYER 2: THE GANG IS STILL COMING. YOU MUST NAVIGATE THROUGH THE CITY MOST EFFICIENTYLY." << endl; 
 cout << "YOU HAVE 2 MIN." << endl; 
 cout << "Press any character to begin" << endl;
@@ -232,7 +178,6 @@ if(cin) {
 	if(choice == 'a') {} 
 } 
 
->>>>>>> 230396e6d64d9e708be835e1855fe32a182976b8
 
 //if riddle is solved...
 cout<<"Congratulations, you are a riddlemaster. Continue to the getaway car."<<endl;
